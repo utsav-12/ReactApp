1389ef4726205bd4aafd5dd6243d97a23c6a5c70
utsav-12 <utsavjain6913@gmail.com>
2021-04-16 02:50:00 +0530

    first React App

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+      imgsrc: 'https://wallpapercave.com/wp/wp4056410.jpg',
+      title: 'A Netflix Orignal Series ',
+      sname: 'DARK',
+      link: 'https://www.netflix.com/in/title/80100172'
+    },
+    {
+      imgsrc: 'https://wallpapercave.com/wp/wp6004697.jpg',
+      title: 'A Netflix Orignal Series ',
+      sname: 'MONEY HEIST',
+      link: 'https://www.netflix.com/in/title/80192098'
+    },
+    {
+      imgsrc: 'https://wallpapercave.com/wp/wp6679399.jpg',
+      title: 'A Netflix Orignal Series ',
+      sname: 'STRANGER THINGS',
+      link: 'https://www.netflix.com/in/title/80057281'
+    },
+    {
+        imgsrc: 'https://wallpapercave.com/wp/wp5275212.jpg',
+        title: 'A Netflix Orignal Series ',
+        sname: 'NARCOS',
+        link: 'https://www.netflix.com/in/title/80025172'
+    },
+    {
+        imgsrc: 'https://wallpapercave.com/wp/wp5680704.jpg',
+        title: 'A Netflix Orignal Series ',
+        sname: 'THE WITCHER',
+        link: 'https://www.netflix.com/in/title/80189685'
+    }
+]
+
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import Card from './Card'
+import Sdata from './Sdata'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+ReactDom.render(
+  <>
+
+  <h1 className = "heading_style">List Of Top Web Series</h1>
+
+    <Card 
+      imgsrc =  {Sdata[0].imgsrc}
+      title  =  {Sdata[0].title}
+      sname  =  {Sdata[0].sname}
+      link   =  {Sdata[0].links}
+    />
+    <Card 
+     imgsrc =  {Sdata[1].imgsrc}
+      title  =  {Sdata[1].title}
+      sname  =  {Sdata[1].sname}
+      link   =  {Sdata[1].links}
+    />
+    <Card 
+     imgsrc =  {Sdata[2].imgsrc}
+      title  =  {Sdata[2].title}
+      sname  =  {Sdata[2].sname}
+      link   =  {Sdata[2].links}
+    />
+     <Card 
+      imgsrc =  {Sdata[3].imgsrc}
+      title  =  {Sdata[3].title}
+      sname  =  {Sdata[3].sname}
+      link   =  {Sdata[3].links}
+    />
+     <Card 
+      imgsrc =  {Sdata[4].imgsrc}
+      title  =  {Sdata[4].title}
+      sname  =  {Sdata[4].sname}
+      link   =  {Sdata[4].links}
+    />
+  </>
+  ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
